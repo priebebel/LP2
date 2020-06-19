9775092edd00e8d95b560200019897abe668f286
priebebel <37842871+priebebel@users.noreply.github.com>
2020-06-19 20:16:23 -0300

    Add files via upload

---
+++ Prova/P0030481721033/Prova/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Prova
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.lstbxCalculos = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(79, 116);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(107, 51);
+            this.btnVerificar.TabIndex = 0;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // lstbxCalculos
+            // 
+            this.lstbxCalculos.FormattingEnabled = true;
+            this.lstbxCalculos.Location = new System.Drawing.Point(258, 35);
+            this.lstbxCalculos.Name = "lstbxCalculos";
+            this.lstbxCalculos.Size = new System.Drawing.Size(259, 238);
+            this.lstbxCalculos.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(602, 295);
+            this.Controls.Add(this.lstbxCalculos);
+            this.Controls.Add(this.btnVerificar);
+            this.Name = "Form1";
+            this.Text = "Prova Pratica";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.ListBox lstbxCalculos;
+    }
+}
+
---
+++ Prova/P0030481721033/Prova/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Prova
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            double[,] Matriz = new double[3, 4];
+            string valor = "";
+            string Semana = "";
+            double mes, Geral = 0;
+
+            for (int lin = 0; lin < 3; lin++)
+            {
+                mes = 0;
+                for (int col = 0; col < 4; col++)
+                {
+                    valor = Interaction.InputBox("Entre com o Faturamento\n" +"Semana:" + (col + 1) +"\n"+ 
+                        "Mes: " + (lin + 1), "Entrada de dados");
+
+                    if (double.TryParse(valor, out Matriz[lin, col]))
+                    {
+                        Semana = "Total do Mês: " + (lin + 1) + " Semana: " + (col + 1) + " R$ = " + Matriz[lin, col].ToString("N2");
+                        mes += Matriz[lin, col];
+                        Geral += Matriz[lin, col];
+                        lstbxCalculos.Items.Add(Semana);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Favor Corrigir os dados");
+                        col--;
+                    }
+                }
+                lstbxCalculos.Items.Add("Total Mês: " + (lin+1) + " R$ = " + mes.ToString("N2"));
+                lstbxCalculos.Items.Add("-----------------------");
+            }
+            lstbxCalculos.Items.Add("Total Geral R$ = " + Geral.ToString("N2"));
+        }
+    }
+}
